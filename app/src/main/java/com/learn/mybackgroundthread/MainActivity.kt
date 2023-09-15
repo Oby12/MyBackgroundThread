9080d63dd9c0cd61438f72326e583521d310da20
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-09-15 16:02:32 +0700

    menambahakan dokumentasi

--- app/src/main/java/com/learn/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/learn/mybackgroundthread/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
         //peggunaan executor unutk menangani lag pada aplikasi
-        /*val executor = Executors.newSingleThreadExecutor()
+        /*val executor = Executors.newSingleThreadExecutor()  //newSingleThreadExecutor yang artinya hanya satu thread yang Anda buat.
         val handler =Handler(Looper.getMainLooper())
 
         btnStart.setOnClickListener {
@@ -60,6 +60,9 @@ class MainActivity : AppCompatActivity() {
                 for (i in 0..10) {
                     delay(500)
                     val percentage = i * 10
+                    //Salah satu kelebihan coroutine yaitu kita bisa berpindah antar thread lebih mudah. Di sini kita menggunakan
+                    // withContext(Dispatchers.Main) karena kita perlu pindah ke Main Thread untuk update UI berupa TextView, jika tidak menggunakan ini,
+                    // maka UI/TextView tidak akan pernah ter-update.
                     withContext(Dispatchers.Main) {
                         //update ui in main thread
                         if (percentage == 100) {
