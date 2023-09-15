56f8e5b9d6a56af898f04b7dadcfe28ce8bcbfcf
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-09-15 15:32:19 +0700

    di sini saya memakai executor dan handler unutk menangani lagging pada aplikasi sebelumnya

--- app/src/main/java/com/learn/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/learn/mybackgroundthread/MainActivity.kt
@@ -2,8 +2,12 @@ package com.learn.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +18,32 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
-        btnStart.setOnClickListener {
-            try {
-                //proes simalasi compres
-                for (i in 0..100){
-                    Thread.sleep(500)
-                    val percentage = i * 10
-                    if (percentage == 100){
-                        tvStatus.setText(R.string.task_completed)
-                    }else{
-                        tvStatus.text = String.format(getString(R.string.compressing),percentage)
-                    }
-                }
+        //peggunaan executor unutk menangani lag pada aplikasi
+        val executor = Executors.newSingleThreadExecutor()
+        val handler =Handler(Looper.getMainLooper())
 
-            }catch (e: InterruptedException){
-                e.printStackTrace()
+        btnStart.setOnClickListener {
+            //ini adlah penggunaan executor
+            executor.execute {
+                try {
+                    //proes simalasi compres
+                    for (i in 0..1000) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        //handler di bawah ini unutk menangani lagging pada aplikasi sebelum nya
+                        handler.post {
+                            if (percentage == 100) {  //-< ini adlah proses compressing sampai dengan 100%
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text =
+                                    String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
             }
 
         }
