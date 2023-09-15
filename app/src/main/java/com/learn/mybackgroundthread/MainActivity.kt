89b940bc1d049b93b15575f8e25c26d2a6a1eb00
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-09-15 15:39:35 +0700

    ini adalah penggunaan couruntine yang merupan penggunaan yang palingn di rekomendasikan

--- app/src/main/java/com/learn/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/learn/mybackgroundthread/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import kotlin.concurrent.thread
@@ -19,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
         //peggunaan executor unutk menangani lag pada aplikasi
-        val executor = Executors.newSingleThreadExecutor()
+        /*val executor = Executors.newSingleThreadExecutor()
         val handler =Handler(Looper.getMainLooper())
 
         btnStart.setOnClickListener {
@@ -46,6 +51,27 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+        }*/
+        //selain menggunaan executor dan handler adalah yang lebih baik yaitu  menggunakan couruntine
+        //penggunaan couruntine harus mengimpelmentasi kan dulu melalui dependencies
+        btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
+                    }
+                }
+            }
         }
+
+
     }
 }
